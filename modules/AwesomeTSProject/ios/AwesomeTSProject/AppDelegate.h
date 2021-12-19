9ae444adce508c496dea01c5e6d5e0907adc2bd4
nacam403 <satnakam@gmail.com>
2021-12-19 16:59:27 +0900

    Firebaseコンソールの案内に従って、AppDelegateを修正。

--- modules/AwesomeTSProject/ios/AwesomeTSProject/AppDelegate.h
+++ modules/AwesomeTSProject/ios/AwesomeTSProject/AppDelegate.h
@@ -1,6 +1,9 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 
+// Firebase用の設定
+@import Firebase;
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
